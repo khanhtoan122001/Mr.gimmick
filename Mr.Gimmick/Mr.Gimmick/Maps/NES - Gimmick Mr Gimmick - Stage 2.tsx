54690700974b8e782e722d0e6a8f5c3e132cb1db
khanhtoan122001 <71191923+khanhtoan122001@users.noreply.github.com>
2021-07-26 02:24:00 +0700

    +map

---
+++ Mr.Gimmick/Mr.Gimmick/Maps/NES - Gimmick Mr Gimmick - Stage 2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="NES - Gimmick Mr Gimmick - Stage 2" tilewidth="16" tileheight="16" tilecount="495" columns="33">
+ <image source="C:/Users/ADMIN/OneDrive/Documents/GitHub/Mr.gimmick/Mr.Gimmick/Mr.Gimmick/Resource/NES - Gimmick Mr Gimmick - Stage 2.png" trans="36a8a7" width="536" height="240"/>
+</tileset>
